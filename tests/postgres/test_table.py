cb107cb38d0ba4a3a75d61813c283330ebb2968c
zac <zac@sqlleaf.io>
2026-06-11 23:46:50 +1000

    test(postgres): separate 'sequence' from 'table'

--- tests/postgres/test_table.py
+++ tests/postgres/test_table.py
@@ -9,7 +9,7 @@ from tests.new_fixtures import holder as holder
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
 
 
-from sqlleaf.objects.query_types import InsertQuery, SequenceQuery, TypeQuery
+from sqlleaf.objects.query_types import TypeQuery
 
 DIALECT = "postgres"
 
@@ -298,28 +298,3 @@ def test__update_only_inherited(holder):
     assert h.paths == [["column[fruit.source_table.price]", "column[fruit.parent_table.price]"]]
     assert len(h.nodes) == 2
     assert len(h.edges) == 1
-
-
-def test__simple_sequence(holder):
-    sql = """
-    CREATE SEQUENCE serial START 101;
-    INSERT INTO fruit.raw (age) SELECT nextval('serial') as age;
-    """
-    h = holder(sql=sql, dialect=DIALECT, with_tables=True)
-
-    assert h.paths == [["sequence[serial]", "column[fruit.raw.age]"]]
-    assert [SequenceQuery, InsertQuery] == list(map(type, h.queries))
-    assert len(h.nodes) == 2
-    assert len(h.edges) == 1
-
-
-def test__temporary_sequence(holder):
-    sql = """
-    CREATE TEMPORARY SEQUENCE temp_serial START 101;
-    INSERT INTO fruit.raw (age) SELECT nextval('temp_serial') as age;
-    """
-    h = holder(sql=sql, dialect=DIALECT, with_tables=True)
-
-    assert h.paths == [["sequence[temp_serial]", "column[fruit.raw.age]"]]
-    assert "sequence[temp_serial type=INT kind=temporary]" in h.nodes_full
-    assert [SequenceQuery, InsertQuery] == list(map(type, h.queries))
