80e44b9b9a866cf639d3ef0c0286f234988c08a3
zac <zac@sqlleaf.io>
2026-07-19 18:35:09 +1000

    test(athena): add external UDFs (unsupported)

---
+++ tests/athena/test_user_defined_function.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+import pytest
+
+from tests.new_fixtures import holder as holder
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
+
+DIALECT = "athena"
+
+
+def test__user_defined_function_lambda(holder):
+    sql = """
+    USING EXTERNAL FUNCTION my_udf(name VARCHAR)
+    RETURNS VARCHAR
+    LAMBDA 'my_lambda_function'
+    SELECT my_udf(expression) FROM fruit.raw;
+    """
+    h = holder(sql=sql, dialect=DIALECT)
+
+    assert h.paths == []
+    assert len(h.collected_queries.unsupported) == 1
+
+
+def test__user_defined_function_sagemaker(holder):
+    sql = """
+    USING EXTERNAL FUNCTION predict_customer_registration(age INTEGER) 
+        RETURNS DOUBLE
+        SAGEMAKER 'xgboost-2019-09-20-04-49-29-303' 
+    SELECT predict_customer_registration(age) AS probability_of_enrolling, customer_id 
+         FROM "sampledb"."ml_test_dataset" 
+         WHERE predict_customer_registration(age) < 0.5;
+    """
+    h = holder(sql=sql, dialect=DIALECT)
+
+    assert h.paths == []
+    assert len(h.collected_queries.unsupported) == 1
