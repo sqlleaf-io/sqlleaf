5b4cd34862d6ee37b7df7dea5487f33de87245d1
zac <zac@sqlleaf.io>
2026-07-27 21:04:16 +1000

    chore: move generator, transformer into their own folder

--- sqlleaf/lineage.py
+++ sqlleaf/lineage.py
@@ -22,7 +22,9 @@ from sqlleaf.models.query import (
     ViewQuery,
 )
 from sqlleaf.path import LineagePath
-from sqlleaf.processors import collector, generator, transformer
+from sqlleaf.processors import collector
+from sqlleaf.processors.generators import generator
+from sqlleaf.processors.transformers import transformer
 from sqlleaf.typing import SqlObjectType
 
 logging.getLogger("sqlglot").setLevel(logging.WARNING)
